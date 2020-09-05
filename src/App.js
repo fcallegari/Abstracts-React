3aed07a16cc3885eb1b8ca7bfa75656e91184885
fabio <fcallegari72@gmail.com>
2020-09-05 16:10:13 +0200

    Pulizie varie
    Pulizia varia prima di applicare il contesto per il cambio lingua

--- src/App.js
+++ src/App.js
@@ -1,142 +1,141 @@
-import React , {Suspense} from 'react';
+import React, {
+	Suspense
+} from 'react';
 import {
-	BrowserRouter as Router, 
-	Route, 
+	BrowserRouter as Router,
+	Route,
 	Switch,
 	Redirect
 } from "react-router-dom";
 import $ from "jquery";
-import Contesto from './common/Contesto.js';
+
 
 export default class App extends React.Component {
-	
-	constructor(props) {
-		super(props);
-		this.state = {lingua: 'IT'};
-		
-    this.cambiaLingua = (l) => {
-			this.setState(() => ({lingua: l }));
-		};
-	}
-	
 	render() {
-		return(
-			<ErrorBoundary>		
-			<Contesto.Provider>
-			<Router basename='/abs_r'>
+			return (
+				<ErrorBoundary>
+
+				<Router basename = '/abs_r'>
 				<Switch>
-					<Route exact path="/:id" 
-					render={
-						(props) => { 
-							return (<Dispatch id={props.match.params.id}/>);
-						}
-					} />
-					<Route path="/">no data here</Route>
+				<Route exact path = "/:id" render =
+				{
+					( props ) => {
+						return ( <Dispatch id = {props.match.params.id}/>);
+					}
+				}/>
+				<Route path = "/"> no data here </Route>
 				</Switch>
-			</Router>
-			</Contesto.Provider>
-			</ErrorBoundary>
+				</Router>
+
+		</ErrorBoundary>
 		)
 	}
 }
 
-class Dispatch extends React.Component {
+		class Dispatch extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.getEvent = this.getEvent.bind(this);
-		this.showForm = this.showForm.bind(this);
-		this.state = {
-			id: this.props.id,
-			evento: null,
-			path: null
-		};
-	}
+				constructor( props ) {
+					super( props );
+					this.getEvent = this.getEvent.bind( this );
+					this.showForm = this.showForm.bind( this );
+					this.state = {
+						id: this.props.id,
+						evento: null,
+						path: null
+					};
+				}
 
-	componentDidMount() {
-		this.getEvent(this.state.id);
-	}
+				componentDidMount() {
+					this.getEvent( this.state.id );
+				}
 
-	showForm() {
-		if (this.state.path) {
-			const Form = React.lazy(() => import (`${this.state.path}`));
-			return ( 
-				<div>
-				<Suspense fallback = { < div > Loading... < /div>}>
-				<Form evento = {this.state.evento}/>
-				</Suspense>
-				</div>
-			)
-		}
-		return <div > Loading... < /div>;
-	}
+				showForm() {
+					if ( this.state.path ) {
+						const Form = React.lazy( () => import( `${this.state.path}` ) );
+						return (
+							<div>
+							<Suspense fallback = {
+								<div> Loading... </div>
+							}>
+							<Form evento = {this.state.evento}/>
+							</Suspense>
+							</div>
+						);
+						}
+						return (<div> Loading... </div>);
+					}
 
-	getEvent(id) {
-		$.ajax({
-			url: "https://adarte.fmhosting.it/Abstracts/getEvent.php",
-			data: {
-				id: id
-			},
-			type: "GET",
-			dataType: "json"
-		})
-		.done((res) => {
-			if (res) {
-				const path = "./forms/" + (res.is_custom ? id : "shared") + "/form.js";
-				this.setState({
-					path: path,
-					evento: res
-				});
-			} else {
-				this.setState(() => {
-					throw new Error("Evento non trovato")
-				});
-			}
-		})
-		.fail((xhr, status, errorThrown) => {
-			console.log("errore fm", xhr);
-			this.setState(() => {
-				throw new Error("missing connection with database")
-			})
-		});
-	}
+				getEvent( id ) {
+						$.ajax( {
+								url: "https://adarte.fmhosting.it/Abstracts/getEvent.php",
+								data: {
+									id: id
+								},
+								type: "GET",
+								dataType: "json"
+							} )
+							.done( ( res ) => {
+								if ( res ) {
+									const path = "./forms/" + ( res.is_custom ? id : "shared" ) + "/form.js";
+									this.setState( {
+										path: path,
+										evento: res
+									} );
+								} else {
+									this.setState( () => {
+										throw new Error( "Evento non trovato" )
+									} );
+								}
+							} )
+							.fail( ( xhr, status, errorThrown ) => {
+								console.log( "errore fm", xhr );
+								this.setState( () => {
+									throw new Error( "missing connection with database" )
+								} )
+							} );
+					}
 
-	render() {
-		if (!this.state.id.match(/^[0-9]+$/)) {
-			return <Redirect to = "/" / > ;
-		}
+				render() {
+						if ( !this.state.id.match( /^[0-9]+$/ ) ) {
+							return <Redirect to = "/" / > ;
+						}
 
-		return (
-			<React.Fragment>
-			{this.showForm()}
-			</React.Fragment>
-		);
-	}
-}
+						return (
+							<React.Fragment>
+								{this.showForm()}
+							</React.Fragment>
+						);
+					}
+				}
 
 class ErrorBoundary extends React.Component {
-	constructor(props) {
-	    super(props);
-	    this.state = { hasError: false, errormsg: null };
-	  }
 
-	static getDerivedStateFromError(error) {
-	    // Update state so the next render will show the fallback UI.
-	    return { 
-				hasError: true, 
-				errormsg: error.message
-			};
-	  }
-		
-	render() {
-		if (this.state.hasError) {
-			return (
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+      errormsg: null
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return {hasError: true, errormsg: error.message};
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
 				<div className="alert alert-danger" role="alert">
-  				<p>Errore applicativo</p>
-					<code>{this.state.errormsg}</code>
-				</div>
-			);
-	  }
-		return this.props.children;
-	 }
+        <p>
+          Errore applicativo
+        </p>
+        <code>
+          {this.state.errormsg}
+        </code>
+      </div>
+		);
+	}
+	return this.props.children;
+  }
 }
--- src/forms/shared/form.js
+++ src/forms/shared/form.js
@@ -1,117 +1,115 @@
 import React from 'react';
-import Contesto from 'src/common/Contesto.js';
 
-export default function Form (props) {
-		return (
-			<React.Fragment>
-			<Header evento={props.evento}>
-			<LanguagePicker/>
-			</Header>
-			<Footer evento={props.evento}/>
-			</React.Fragment>
-		);	
+export default function Form( props ) {
+	return (
+		<React.Fragment>
+	<Header evento={ props.evento }>
+
+	</Header>
+	<Footer evento={ props.evento }/>
+	</React.Fragment>
+	);
 }
 
-function LanguagePicker() {
-	return(
-		<Contesto.Consumer>
-		<div className="btn-group" role="group" aria-label="Choose language">
-		<button type="submit" name="lang" value="IT" className="btn btnLink">IT</button>
-		<button type="submit" name="lang" value="EN" className="btn btnLink">EN</button>
-		</div>
-		</Contesto.Consumer>
-	)
-}
+// function LanguagePicker() {
+// 	return (
+// 		<Contesto.Consumer>
+// 			<div className="btn-group" role="group" aria-label="Choose language">
+// 				<button type="submit" name="lang" value="IT" className="btn btnLink"> IT </button>
+// 				<button type="submit" name="lang" value="EN" className="btn btnLink"> EN </button>
+// 			</div>
+// 		</Contesto.Consumer>
+// 	)
+// }
 
-function Header(props){
+function Header( props ) {
 	const evento = props.evento;
-	return(
-<React.Fragment>
-	<nav className="navbar navbar-light" style={{backgroundColor: "#5fbcb4"}}>
-		<img className="navbar-brand" src={process.env.PUBLIC_URL + '/img/logo.png'} alt="logo"></img>
+	return (
+		<React.Fragment>
+	<nav className="navbar navbar-light" style={{
+    backgroundColor: "#5fbcb4"
+  }}>
+		<img className="navbar-brand" src={process.env.PUBLIC_URL + '/img/logo.png' } alt="logo"></img>
 		{props.children}
 	</nav>
 	<br/>
 	<div className="container">
 		<div className="card shadow text-center">
 			<div className="card-body m-2">
-				<h5 className="card-title">{evento.titoloEvento}</h5>
-				<h6 className="card-subtitle mb-2 text-muted">{evento.sottotitolo}</h6>
-				<p className="card-text m-0">{Array.of(evento.sede_nome , evento.c_tagDurata).filter((e)=> e.length>0).join(', ')}</p>
-				<p className="card-text m-0">{Array.of(evento.sede_indirizzo , evento.sede_Comune  + (evento.sede_Prov && ` (${evento.sede_Prov})`)).filter((e)=> e.length>0).join(' - ')}</p>
+				<h5 className="card-title">
+					{evento.titoloEvento}</h5>
+				<h6 className="card-subtitle mb-2 text-muted">
+					{evento.sottotitolo}
+				</h6>
+				<p className="card-text m-0">
+					{Array.of(evento.sede_nome, evento.c_tagDurata).filter((e) => e.length > 0).join(', ')}
+				</p>
+				<p className="card-text m-0">
+					{Array.of(evento.sede_indirizzo, evento.sede_Comune + (evento.sede_Prov && ` (${evento.sede_Prov})`)).filter((e) => e.length > 0).join(' - ')}
+				</p>
 			</div>
 		</div>
 	</div>
 </React.Fragment>
-	);
+);
 }
 
-function Footer(props){
+function Footer( props ) {
 	const evento = props.evento;
-	return(
-	<React.Fragment>
-	<hr/>
-	<footer style={{fontSize:12}}>
-	
-	<div className="container">
-	   <div className="row justify-content-center">
-	     <div className="col-auto">
-		Segreteria Organizzativa
-	</div>
-	</div>
-
-	<div className="row justify-content-center">
-	<div className="col-auto">
-		<a href="https://adarteventi.com"><img src={process.env.PUBLIC_URL + '/img/logo.png'} alt="AdArte Eventi" style={{width:120}}/></a>
-	</div>
-	</div>
-		</div>
-		
-				<div className="row justify-content-center">
-				<div className="col-auto">
-				<a href={`http://maps.google.com/?q=${evento.segr_indirizzo} ${evento.segr_CAP} ${evento.segr_Comune}`} target="_blank" rel="noopener noreferrer" style={{display:"inline"}}>
-				<i className="fas fa-map-marker-alt"></i>
-				</a>
-				{' '}{evento.segr_nome}
-			</div>
-		</div>
-		
-		<div className="row justify-content-center">
-			<div className="col-auto">{evento.segr_indirizzo}</div>
-			</div>
-						<div className="row justify-content-center">
-							<div className="col-auto">{evento.segr_CAP} {evento.segr_Comune} ({evento.segr_Prov})</div>
-						</div>	
-						
-				{evento.segr_Tel && 
-					<div className="row justify-content-center">
-					<div className="col-auto"><i className="fas fa-phone-alt"></i>{' '}{evento.segr_Tel}</div>
-				</div>
-				}
-
-				{evento.segr_Fax &&
-					<div className="row justify-content-center">
-					<div className="col-auto"><i className="fas fa-fax"></i>{' '}{evento.segr_Fax}</div>
-				</div>
-				}
-
-				{evento.segr_Email &&
-				<div className="row justify-content-center">
-				<div className="col-auto"> <i className="fas fa-paper-plane"></i>
-					<a href={`mailto:${evento.segr_Email}`}>{' '}{evento.segr_Email}</a></div>
-				</div>
-				}
-
-				{evento.segr_Web &&
+	return ( <React.Fragment>
+	<hr />
+	<footer style={{fontSize: 12}}>
+		<div className="container">
 			<div className="row justify-content-center">
-				<div className="col-auto"><i className="fas fa-globe"></i>
-				 <a href={`http://${evento.segr_Web}`} target="_blank" rel="noopener noreferrer">{' '}{evento.segr_Web}</a></div>
-			 </div>	
-				}
+				<div className="col-auto">
+					Segreteria Organizzativa </div>
+			</div>
 
-		</footer>
-	</React.Fragment>
+			<div className="row justify-content-center">
+				<div className="col-auto">
+					<a href="https://adarteventi.com"> <img src={process.env.PUBLIC_URL + '/img/logo.png' } alt="AdArte Eventi" style={{width: 120}} /></a>
+				</div>
+			</div>
+		</div>
+
+		<div className="row justify-content-center">
+			<div className="col-auto"><a href={`http://maps.google.com/?q=${evento.segr_indirizzo} ${evento.segr_CAP} ${evento.segr_Comune}`} target="_blank" rel="noopener noreferrer" style={{display: "inline"}}>
+					<i className="fas fa-map-marker-alt"> </i> </a> {' '} {evento.segr_nome} </div>
+		</div>
+		<div className="row justify-content-center">
+			<div className="col-auto"> {evento.segr_indirizzo} </div>
+		</div>
+		<div className="row justify-content-center">
+			<div className="col-auto"> {evento.segr_CAP} {evento.segr_Comune}( {evento.segr_Prov} ) </div>
+		</div>
+
+		{ evento.segr_Tel &&
+		<div className="row justify-content-center">
+			<div className="col-auto"> <i className="fas fa-phone-alt"> </i>{' '}{evento.segr_Tel}</div>
+		</div>
+		}
+
+		{ evento.segr_Fax &&
+		<div className="row justify-content-center">
+			<div className="col-auto"> <i className="fas fa-fax"> </i>{' '}{evento.segr_Fax}</div>
+		</div>
+		}
+
+		{ evento.segr_Email &&
+		<div className="row justify-content-center">
+			<div className="col-auto"> <i className="fas fa-paper-plane"> </i>
+				<a href={`mailto:${evento.segr_Email}`}> {' '} {evento.segr_Email} </a></div>
+		</div>
+		}
+
+		{ evento.segr_Web &&
+		<div className="row justify-content-center">
+			<div className="col-auto"> <i className="fas fa-globe"> </i>
+				<a href={`http://${evento.segr_Web}`} target="_blank" rel="noopener noreferrer"> {' '} {evento.segr_Web} </a></div>
+		</div>
+		}
+
+	</footer>
+</React.Fragment>
 	);
 }
-
-
