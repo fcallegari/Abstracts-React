cfba21973570482ade0ba38133fcb7435cb26f6e
fabio <fcallegari72@gmail.com>
2020-09-14 10:21:50 +0200

    importato react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js'
-import 'bootstrap/dist/css/bootstrap.css';
+import App from './App.js';
 
 // ========================================
 
-ReactDOM.render(
-	<App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render( <App/> , document.getElementById( 'root' ));
