0f3e926c6b7266c18709bad17011fd503aba9121
fabio <fcallegari72@gmail.com>
2020-09-14 10:24:31 +0200

    wrappati tutti i sottocomponenti dentro un unico consumer

--- src/forms/shared/form.js
+++ src/forms/shared/form.js
@@ -1,28 +1,30 @@
 import React from 'react';
-import {Header, LanguageSwitcher, Footer} from './assets.js';
+import {Header, LanguageSwitcher, Footer, EventoScaduto} from './assets.js';
 import * as i18n from '../../i18n/i18n.js';
 
 export default function Form( props ) {
-const evento=props.evento;
+	const evento=props.evento;
+	const scadenza = new Date(evento["Desk::dataScadenzaRegistrazioni"]);
+	const oggi = new Date();
+	oggi.setHours(0,0,0,0);
 	return (
 		<i18n.ProviderLingua evento={evento}>
-			<i18n.ConsumerLingua>
+			<i18n.ContestoLingua.Consumer>
 					{contesto =>
-						(<React.Fragment>
+						(<div className="container">
 							<Header contesto={ contesto }>
 								<LanguageSwitcher contesto={contesto}/>
 							</Header>
-							<FormBody contesto={contesto}/>
+							{oggi > scadenza ? <EventoScaduto contesto={contesto}/> : <FormBody contesto={contesto}/>}
 							<Footer contesto={ contesto }/>
-						</React.Fragment>
+						</div>
 					)}
-				</i18n.ConsumerLingua>
+				</i18n.ContestoLingua.Consumer>
 		</i18n.ProviderLingua>
 	);
 }
 
 function FormBody(props) {
-	const t = props.t;
-
-return null;
+	const {t} = props.contesto;
+	return (<h3> {t("privacy_link")} </h3>);
 }
